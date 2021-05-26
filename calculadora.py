814f0abb6e4cd517a02538544c288a47ade41584
caio <caiobrone77@gmail.com>
2021-05-26 04:52:00 -0300

    adequacao das funcoes para passar nos testes

--- calculadora.py
+++ calculadora.py
@@ -1,12 +1,50 @@
-operator = {
-    '': lambda a, b: a+b, 
-    '2': lambda a, b: a-b, 
-    '4': lambda a, b: a/b, 
-    '3': lambda a, b: a*b,
-    '5': lambda a, b: a**b,
+
+def all_values_is_numbers(a, b):
+    return isinstance(a, int) and isinstance(b, int)
+
+def somar(a,b):
+    if all_values_is_numbers(a, b):
+        return a+b
+    return 'ambos os valores devem ser numberos'
+
+def sub(a,b):
+    return a-b
+    if all_values_is_numbers(a, b):
+        return a+b
+    return 'ambos os valores devem ser numberos'
+
+
+def mult(a,b):
+    if all_values_is_numbers(a, b):
+        return a*b
+    return 'ambos os valores devem ser numberos'
+
+
+def div(a,b):
+    two_number_equal_zero = a == 0 and b == 0
+    if two_number_equal_zero:
+        return 'nao pode dividir por 0'
+
+    if all_values_is_numbers(a, b):
+        return a/b
+    return 'ambos os valores devem ser numberos'
+
+
+def pot(a,b):
+    if all_values_is_numbers(a, b):
+        return a**b
+    return 'ambos os valores devem ser numberos'
+
+
+mathematical_formulas = {
+    '1': somar, 
+    '2': sub, 
+    '4': mult, 
+    '3': div,
+    '5': pot,
     }
 
-def calculator():
+def operations():
     print('''
     1 - Somar
     2 - Subtrair
@@ -16,15 +54,9 @@ def calculator():
     ''')
     return input('Informe o numero da operação que deseja realizar: ')
 
-def main():
+def main(a, b):
     while True:
         sair = int(input('1 - Sair | 0 - Continuar: '))
         if sair == 1:
             break
-        a = int(input('Informe o valor de A: '))
-        b = int(input('Informe o valor de B: '))
-        return operator[calculator()](a,b)
-        
-
-if __name__ == '__main__':
-    print(main())
+        return mathematical_formulas[operations()](a, b)
